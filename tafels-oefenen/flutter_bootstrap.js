9d36436ea827616f80079fb177971ca85422028f
thpir <92860191+thpir@users.noreply.github.com>
2025-12-27 15:05:16 +0100

    updated math facts app

--- tafels-oefenen/flutter_bootstrap.js
+++ tafels-oefenen/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1452548882"
+    serviceWorkerVersion: "3040182396"
   }
 });
